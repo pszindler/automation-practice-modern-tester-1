652dd02f680bd5397e5045a55b5fa917365df345
Patryk <pszindler@sii.pl>
2022-09-13 16:51:42 +0200

    autosuggest

--- src/main/java/pageObjects/AutocompletePage.java
+++ src/main/java/pageObjects/AutocompletePage.java
@@ -4,24 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class AutocompletePage extends BasePage {
 
-    AutocompletePage(WebDriver driver) {
+    public AutocompletePage(WebDriver driver) {
         super(driver);
     }
 
     @FindBy(css = "input#search")
     private WebElement inputSearch;
 
-    @FindBy(css = "")
-    private WebElement autoSuggest;
+    @FindBy(xpath = "//body/ul/li[@class='ui-menu-item']")
+    private List<WebElement> autoSuggest;
 
     public void sendKeys(String text) {
         inputSearch.sendKeys(text);
     }
 
     public void printAutoSuggest() {
-
+        for (WebElement suggest: autoSuggest) {
+            System.out.println(suggest.getText());
+        }
     }
 
     public void selectRandomOption() {
--- src/test/java/widgetsTestModule/AutocompletePageTest.java
+++ src/test/java/widgetsTestModule/AutocompletePageTest.java
@@ -1,7 +1,17 @@
 package widgetsTestModule;
 
 import base.BaseTest;
+import com.beust.ah.A;
+import org.junit.jupiter.api.Test;
+import pageObjects.AutocompletePage;
 
 public class AutocompletePageTest extends BaseTest {
 
+    @Test
+    void test() {
+        AutocompletePage autocompletePage = new AutocompletePage(driver);
+        driver.get("https://seleniumui.moderntester.pl/autocomplete.php");
+        autocompletePage.sendKeys("a");
+        autocompletePage.printAutoSuggest();
+    }
 }
